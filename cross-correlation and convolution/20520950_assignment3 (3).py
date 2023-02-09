4071cb53ae7381ed28493afcc659dc5bc2f018b2
vungods <ngoducvu2020@gmail.com>
2023-02-10 02:19:43 +0700

    updater another cross_correlation_2D

--- cross-correlation and convolution/20520950_assignment3 (3).py
+++ cross-correlation and convolution/20520950_assignment3 (3).py
@@ -17,11 +17,16 @@ def convolution_2D(img,ker):
     ker = ker[::-1,::-1]
     return cross_correlation_2D(img,ker)
 
+def cross_correlation_2D2(img,ker):
+    result = np.zeros((img.shape[0] - ker.shape[0] + 1, img.shape[1] - ker.shape[1] + 1))
+    for i in range(result.shape[0]):
+        for j in range(result.shape[1]):
+            result[i, j] = np.sum(img[i:i+ker.shape[0], j:j+ker.shape[1]] * ker)
+    return result
 if __name__ == "__main__":
     img = np.array([[1,2,3,4,5,6,1],
     [4,5,6,73,6,4,1],[3,4,6,3,3,1,6],[3,45,3,5,4,1,6],[9,6,4,4,3,1,3],[12,4,3,1,5,4,3]
     ,[12,4,3,1,5,4,3]])
     ker = np.array([[1,0,0], [0,1,0], [0,0,1]])
-    re_cross = cross_correlation_2D(img,ker)
+    re_cross = cross_correlation_2D2(img,ker)
     print(re_cross)
-
