cc9b9e1c281759b685a0ce5649022ab49383394f
vungods <ngoducvu2020@gmail.com>
2023-02-10 19:15:50 +0700

    histogram

--- histogram/20520950_Assignment8/histogram function.py
+++ histogram/20520950_Assignment8/histogram function.py
@@ -2,24 +2,18 @@ import cv2
 import numpy as np
 # image=np.array([[1,2,4,2],[1,3,2,2],[1,1,2,2]])
 image=cv2.imread('lowconstract.png')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-unique, count=np.unique(image,return_counts=True)
-print(unique,count)
+image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+unique,count = np.unique(image,return_counts=True)
 cdf,hv=[],[]
 sum=0
-for i in range (0,len(unique)):
-    sum+=count[i]
-    cdf.append(sum)
-for i in range (0,len(unique)):
+# for i in range(len(unique)):
+#     sum+=count[i]
+#     cdf.append(sum)
+for i in range(len(unique)):
     sum+=count[i]
     cdf.append(sum)
     hv.append(round((cdf[i]-cdf[0])/(image.shape[0]*image.shape[1]-cdf[0])*255))
-xmax,ymax= np.shape(image)
-for x in range (xmax):
-    for y in range(ymax):
-        for i in range (0,len(unique)):
-                if image[x][y] == unique[i]:
-                    image[x][y] = hv[i]
-                    break
+for i in range(len(unique)):
+    image = np.where(image == unique[i], hv[i], image)
 cv2.imshow('img',image)
 cv2.waitKey(0)
